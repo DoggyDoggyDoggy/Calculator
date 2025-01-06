0317fa278b320b4a5369bd5ea0b0f1a401b67f19
Doggy <pedan.denys@gmail.com>
2025-01-07 05:40:56 +1300

    Simple Calculator

---
+++ app/src/main/java/denys/diomaxius/calculator/MainActivity.kt
@@ -0,0 +1,36 @@
+package denys.diomaxius.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import denys.diomaxius.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val calculatorViewModel = ViewModelProvider(this)[CalculatorViewModel::class.java]
+
+        setContent {
+            CalculatorTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Calculator(
+                        modifier = Modifier.padding(innerPadding),
+                        viewModel = calculatorViewModel
+                    )
+                }
+            }
+        }
+    }
+}
